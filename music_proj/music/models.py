34806c425d19ea8abe8f83f0ac476f0966cdaa1a
DAVID paredes <augustspies22@gmail.com>
2021-07-14 09:54:56 -0700

    model Change

--- music_proj/music/models.py
+++ music_proj/music/models.py
@@ -8,4 +8,5 @@ class Song(models.Model):
     title = models.CharField(max_length=50)
     artist = models.CharField(max_length=50)
     album = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
     release_date = models.CharField(max_length=100)
